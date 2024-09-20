e7cc16f8b048cef22de9626264cace2faa7670d9
6kumaran <88043642+6kumaran@users.noreply.github.com>
2024-09-20 22:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def main():
     video_placeholder = st.empty()
     
     if run:
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
 
         if not cap.isOpened():
             st.error("Error opening video stream or file")
